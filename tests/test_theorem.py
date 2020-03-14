00cc52f2dfc76ea66c438e393e5a2072c83c8a6c
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-03-14 15:55:01 +0100

    Extend test for theorem environment

--- tests/test_theorem.py
+++ tests/test_theorem.py
@@ -30,7 +30,20 @@ Z
 def test_theorem():
     toks = p.parse(latex_theorem)
     plain, pos = utils.get_txt_pos(toks)
-
     assert plain_theorem == plain
 
+
+latex_option = r"""
+X\begin{theorem}[\textcolor{red}{blue}]Y
+"""
+plain_option = r"""
+X
+
+Theorem (blue).
+Y
+"""
+def test_option():
+    toks = p.parse(latex_option)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain_option == plain
 
