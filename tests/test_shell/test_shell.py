2cde58c689e91135a3f2e7cee6d88cd334d75733
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-03-22 18:52:47 +0100

    Added test for sub-module yalafi.shell

---
+++ tests/test_shell/test_shell.py
@@ -0,0 +1,54 @@
+
+#
+#   test of shell.py with own "LT server"
+#
+
+import subprocess
+import time
+import urllib.request
+
+directory = 'tests/test_shell/'
+file = directory + 'latex'
+
+server_cmd = 'python ' + directory + 'lt-server.py'
+shell_cmd = 'python -m yalafi.shell --server my ' + file + '.tex'
+shell_cmd_html = shell_cmd + ' --html'
+server_stop = 'http://localhost:8081/stop'
+
+
+def test_shell():
+
+    # start "LT server"
+    subprocess.Popen(server_cmd.split(), stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL)
+    time.sleep(10)
+
+    # run shell.py
+    out = subprocess.run(shell_cmd.split(), stdout=subprocess.PIPE,
+                        stderr=subprocess.DEVNULL)
+    out_txt = out.stdout.decode('utf-8')
+
+    # run shell.py --html
+    out = subprocess.run(shell_cmd_html.split(), stdout=subprocess.PIPE,
+                        stderr=subprocess.DEVNULL)
+    out_html = out.stdout.decode('utf-8')
+
+    # stop server
+    data = 'x'.encode('ascii')
+    requ = urllib.request.Request(server_stop, data)
+    try:
+        f = urllib.request.urlopen(requ)
+        f.close()
+    except:
+        pass
+
+    f = open(file + '.txt')
+    expect_txt = f.read()
+    f.close()
+    f = open(file + '.html')
+    expect_html = f.read()
+    f.close()
+
+    assert out_txt == expect_txt
+    assert out_html == expect_html
+
