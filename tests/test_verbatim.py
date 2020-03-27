a87d6a5b6b0359b679ccacd7d5c7115ef5f69d63
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-03-27 18:35:18 +0100

    Added tests for verbatim

--- tests/test_verbatim.py
+++ tests/test_verbatim.py
@@ -4,7 +4,7 @@
 #   - test of \verb and \begin{verbatim}
 #
 
-from yalafi import tex2txt
+from yalafi import tex2txt, defs, parameters, parser, utils
 
 options = tex2txt.Options(lang='en', char=True)
 
@@ -21,4 +21,55 @@ def test_verbatim():
     latex = 'A\\begin{verbatim}\\verb?%\\x?\n\\end{verbatim}B'
     plain, nums = tex2txt.tex2txt(latex, options)
     assert plain == 'A\n\n\\verb?%\\x?\n\n\nB'
+
+latex_1 = r"""
+X\begin{verbatim}
+Y
+\end{verbatim}Z
+"""
+plain_1 = r"""
+X
+
+Y
+
+
+Z
+"""
+def test_1():
+    # keep verbatim
+    parms = parameters.Parameters()
+    p = parser.Parser(parms)
+    toks = p.parse(latex_1)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain == plain_1
+
+plain_2 = r"""
+XZ
+"""
+def test_2():
+    # remove verbatim
+    parms = parameters.Parameters()
+    parms.environment_defs.append(defs.Environ(parms, 'verbatim',
+                                    remove=True, add_pars=False))
+    p = parser.Parser(parms)
+    toks = p.parse(latex_1)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain == plain_2
+
+plain_3 = r"""
+X
+
+[verbatim]
+
+Z
+"""
+def test_3():
+    # replace verbatim
+    parms = parameters.Parameters()
+    parms.environment_defs.append(defs.Environ(parms, 'verbatim',
+                            repl='[verbatim]', remove=True))
+    p = parser.Parser(parms)
+    toks = p.parse(latex_1)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain == plain_3
 
