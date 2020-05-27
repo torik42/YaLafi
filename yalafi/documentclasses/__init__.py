82d2d65fd7129c4ee30aa834fc603eae5c80ff9c
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-05-27 21:19:51 +0200

    Addresses #33

--- yalafi/documentclasses/__init__.py
+++ yalafi/documentclasses/__init__.py
@@ -1,1 +1,1 @@
-
+load_table = {}
---
+++ yalafi/documentclasses/scrreprt.py
@@ -0,0 +1,12 @@
+
+#
+#   YaLafi: \documentclass{scrreprt}
+#
+
+from yalafi.defs import ModParm
+
+require_packages = []
+
+def modify_parameters(parms):
+    return ModParm()
+
