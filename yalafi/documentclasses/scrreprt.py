df300fdb2d985c21c1561d35be8442b296d2ef10
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-05-28 15:59:38 +0200

    Added test macro to scrreprt

--- yalafi/documentclasses/scrreprt.py
+++ yalafi/documentclasses/scrreprt.py
@@ -8,5 +8,10 @@ from yalafi.defs import ModParm
 require_packages = []
 
 def modify_parameters(parms):
-    return ModParm()
+
+    macros_latex = r"""
+        \newcommand{\KOMAoptions}[1]{}
+    """
+
+    return ModParm(macros_latex=macros_latex)
 
