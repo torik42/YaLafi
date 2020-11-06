a5acf784fb9b24150f114e0dcf36b4737285f5a5
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-11-06 10:54:43 +0100

    core: added multi-language support, #98

--- yalafi/packages/babel.py
+++ yalafi/packages/babel.py
@@ -3,23 +3,89 @@
 #   YaLafi module for LaTeX package babel
 #
 
-from yalafi.defs import ModParm
+#   map between language codes for babel and LanguageTool
+#   - sorted according to xx-XX code
+#
+language_map = {
+#   'ar'
+#   'ast-ES'
+    'belarusian': 'be-BY',
+    'breton': 'br-FR',
+    'catalan': 'ca-ES',
+#   'ca-ES-valencia'
+    'danish': 'da-DK',
+    'german': 'de-DE',
+    'german-at': 'de-AT',
+    'german-ch': 'de-CH',
+    'greek': 'el-GR',
+    'english': 'en-GB',
+    'english-au': 'en-AU',
+    'english-ca': 'en-CA',
+    'english-gb': 'en-GB',
+    'english-nz': 'en-NZ',
+    'english-us': 'en-US',
+#   'en-ZA',
+    'esperanto': 'eo',
+    'spanish': 'es',
+#   'fa',
+    'french': 'fr',
+#   'ga-IE'
+    'galician': 'gl-ES',
+    'italian': 'it',
+    'japanese': 'ja-JP',
+#   'km-KH'
+    'dutch': 'nl',
+#   'nl-BE'
+    'polish': 'pl-PL',
+    'portuguese': 'pt-PT',
+#   'pt-AO'
+    'portuguese-br': 'pt-BR',
+#   'pt-MZ'
+    'romanian': 'ro-RO',
+    'russian': 'ru-RU',
+    'slovak': 'sk-SK',
+    'slovenian': 'sl-SI',
+    'swedish': 'sv',
+    'tamil': 'ta-IN',
+#   'tl-PH'
+    'ukrainian': 'uk-UA',
+    'chinese': 'zh-CN',
+}
+
+from yalafi.defs import ModParm, Macro, LanguageToken
 
 require_packages = []
 
 def modify_parameters(parms):
 
-    macros_latex = r"""
+    macros_latex = ''
 
-        \newcommand{\foreignlanguage}[3][]{#3}
-        \newcommand{\selectlanguage}[1]{}
+    macros_python = [
 
-    """
+        Macro(parms, '\\foreignlanguage', args='OAA', repl=h_foreignlanguage),
+        Macro(parms, '\\selectlanguage', args='A', repl=h_selectlanguage),
 
-    macros_python = []
+    ]
 
     environments = []
 
     return ModParm(macros_latex=macros_latex, macros_python=macros_python,
                         environments=environments)
+
+def modify_language_map(babel, lt):
+    language_map[babel] = lt
+
+#   translate babel code to LanguageTool code
+#
+def translate_lang(lang):
+    return language_map.get(lang, language_map['english'])
+
+def h_foreignlanguage(parser, buf, mac, args, pos):
+    lang = translate_lang(parser.get_text_expanded(args[1]).strip())
+    return ([LanguageToken(pos, lang=lang)] + args[2]
+                        + [LanguageToken(args[2][-1].pos, back=True)])
+
+def h_selectlanguage(parser, buf, mac, args, pos):
+    lang = translate_lang(parser.get_text_expanded(args[0]).strip())
+    return [LanguageToken(pos, lang=lang, hard=True)]
 
