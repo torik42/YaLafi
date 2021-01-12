d9a53c0a99a03f6e7830fcb8088af2d1c917c77d
Michael Behrens <mfbehrens99@gmail.com>
2021-01-12 23:35:43 +0100

    Created entry point for exe

--- yalafi/shell/__main__.py
+++ yalafi/shell/__main__.py
@@ -1,3 +1,7 @@
-import sys
-sys.argv[0] = 'yalafi.shell'
-from . import shell
+def main():
+    import sys
+    sys.argv[0] = 'yalafi.shell'
+    from . import shell
+
+if __name__ == '__main__':
+    main()
