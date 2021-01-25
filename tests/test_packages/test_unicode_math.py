fa8a31dbc36d92eeb73a59f515987a60cce7875e
torik <77811180+torik42@users.noreply.github.com>
2021-01-25 15:38:21 +0100

    add test for display math with unicode_math

--- tests/test_packages/test_unicode_math.py
+++ tests/test_packages/test_unicode_math.py
@@ -1,6 +1,50 @@
 import pytest
 from yalafi import parameters, parser, utils
 
+
+latex_1 = r"""
+\usepackage{amsmath}
+\usepackage{unicode-math}
+A
+\begin{align}
+    a &≤ b \\
+    -c &≥ d.
+\end{align}
+B
+"""
+plain_1 = r"""
+A
+  V-V-V  equal W-W-W
+  W-W-W  equal X-X-X.
+B
+"""
+
+def test_1():
+    p = parser.Parser(parameters.Parameters())
+    toks = p.parse(latex_1)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain_1 == plain
+
+
+latex_2 = r"""
+\usepackage{amsmath}
+\usepackage{unicode-math}
+\begin{align}
+    a &= b, \\
+    -c &= d
+\end{align}
+"""
+plain_2 = r"""
+  V-V-V  equal W-W-W,
+  X-X-X  equal Y-Y-Y
+"""
+def test_2():
+    p = parser.Parser(parameters.Parameters())
+    toks = p.parse(latex_2)
+    plain, pos = utils.get_txt_pos(toks)
+    assert plain_2 == plain
+
+
 preamble = '\\usepackage{unicode-math}\n'
 
 def get_plain(latex):
