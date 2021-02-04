082d0209e930c746bd209d7a1a6cfaa7636f0aeb
Matthias Baumann <matthias.baumann@tu-dresden.de>
2021-02-04 07:13:00 +0100

    Closes #185

--- yalafi/packages/subfiles.py
+++ yalafi/packages/subfiles.py
@@ -1,29 +1,25 @@
 
 #
-#   YaLafi module for LaTeX package tikz
+#   YaLafi module for LaTeX package subfile
 #
 
 from yalafi.defs import Environ, InitModule
 
-require_packages = ['graphicx']
+require_packages = []
 
 def init_module(parser, options, position):
     parms = parser.parms
 
     macros_latex = r"""
 
-        \newcommand{\tikzset}[1]{}
-        \newcommand{\usetikzlibrary}[1]{}
+        \newcommand{\subfile}[1]{}
+        \newcommand{\subfileinclude}[1]{}
 
     """
 
     macros_python = []
 
-    environments = [
-
-        Environ(parms, 'tikzpicture', remove=True, add_pars=False),
-
-    ]
+    environments = []
 
     return InitModule(macros_latex=macros_latex, macros_python=macros_python,
                         environments=environments)
