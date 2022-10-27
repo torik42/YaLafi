cc8b25d96452617d56811ae582c6b01b54cf7f49
torik <77811180+torik42@users.noreply.github.com>
2022-10-27 18:05:33 +0200

    add tests for new version of glossaries

--- tests/test_packages/test_glossaries.py
+++ tests/test_packages/test_glossaries.py
@@ -3,22 +3,32 @@
 import pytest
 from yalafi import parameters, parser, utils
 
-preamble = ('\\usepackage{glossaries}\n'
-                + '\\LTinput{tests/test_packages/glossaries.glsdefs}\n')
+preamble_pre_4_47 = (
+    '\\usepackage{glossaries}\n'
+    + '\\LTinput{tests/test_packages/glossaries_pre_4_47.glsdefs}\n')
 
-def get_plain(latex):
-    def read(file):
-        try:
-            with open(file) as f:
-                return True, f.read()
-        except:
-            return False, ''
-    parms = parameters.Parameters()
-    p = parser.Parser(parms, read_macros=read)
-    plain, nums = utils.get_txt_pos(p.parse(preamble + latex, source='t.tex'))
-    assert len(plain) == len(nums)
-    return plain
+preamble_post_4_47 = (
+    '\\usepackage{glossaries}\n'
+    + '\\LTinput{tests/test_packages/glossaries_post_4_47.glsdefs}\n')
 
+def gen_get_plain(preamble):
+    def get_plain(latex):
+        def read(file):
+            try:
+                with open(file) as f:
+                    return True, f.read()
+            except:
+                return False, ''
+        parms = parameters.Parameters()
+        p = parser.Parser(parms, read_macros=read)
+        plain, nums = utils.get_txt_pos(
+            p.parse(preamble + latex, source='t.tex'))
+        assert len(plain) == len(nums)
+        return plain
+    return get_plain
+
+get_plain_versions = [gen_get_plain(preamble_pre_4_47),
+                      gen_get_plain(preamble_post_4_47)]
 
 data_test_macros_latex = [
 
@@ -63,7 +73,8 @@ data_test_macros_latex = [
 ]
 
 @pytest.mark.parametrize('latex,plain_expected', data_test_macros_latex)
-def test_macros_latex(latex, plain_expected):
+@pytest.mark.parametrize('get_plain', get_plain_versions)
+def test_macros_latex(latex, plain_expected, get_plain):
     plain = get_plain(latex)
     assert plain == plain_expected
 
@@ -76,10 +87,10 @@ plain_1 = """
 stderr_1 = r"""*** LaTeX error: code in 't.tex', line 4, column 1:
 *** could not find label for \gls... - did you include "\LTinput{<main file>.glsdefs}"?
 """
-def test_1(capsys):
+@pytest.mark.parametrize('get_plain', get_plain_versions)
+def test_1(capsys, get_plain):
     capsys.readouterr()
     plain = get_plain(latex_1)
     cap = capsys.readouterr()
     assert plain == plain_1
     assert cap.err == stderr_1
-
