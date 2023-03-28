e390130c423e0f2d2660a31c4b1acafd6b504a81
torik42 <77811180+torik42@users.noreply.github.com>
2023-03-28 11:10:34 +0200

    Refactor some tests for consistency

--- tests/test_newcommand.py
+++ tests/test_newcommand.py
@@ -8,35 +8,35 @@ from yalafi import parameters, parser, utils
 
 p = parser.Parser(parameters.Parameters())
 
-latex1 = r"""
+latex_1 = r"""
 \newcommand{\xxx}[1][X]{#1Z}
 \xxx
 \xxx
 [A]
 """
-plain1_should_be = r"""
+plain_1 = r"""
 XZAZ
 """
 def test_1():
-    toks = p.parse(latex1)
-    plain1, pos1 = utils.get_txt_pos(toks)
+    toks = p.parse(latex_1)
+    plain, pos = utils.get_txt_pos(toks)
 
-    assert plain1_should_be == plain1
+    assert plain == plain_1
 
-latex2 = r"""
+latex_2 = r"""
 \newcommand{\xxx}[2][X]{#2#1}
 \xxx{ A }
 \xxx[B]C
 """
-plain2_should_be = r"""
+plain_2 = r"""
  A X
 CB
 """
 def test_2():
-    toks = p.parse(latex2)
-    plain2, pos2 = utils.get_txt_pos(toks)
+    toks = p.parse(latex_2)
+    plain, pos = utils.get_txt_pos(toks)
 
-    assert plain2_should_be == plain2
+    assert plain == plain_2
 
 
 latex_3 = r"""
